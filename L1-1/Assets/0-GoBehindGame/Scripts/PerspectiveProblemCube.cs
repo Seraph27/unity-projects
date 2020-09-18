5b137a3b5d1cd40aa2fd339f05e6a56c3e6335e1
rexchao27 <rexchao27@gmail.com>
2020-09-18 18:35:44 +0800

    large commit with assets

---
+++ L1-1/Assets/0-GoBehindGame/Scripts/PerspectiveProblemCube.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveProblemCube : MonoBehaviour
+{
+
+    public bool wobble;
+
+    float speed = 4;
+    float scaleIncrease = .5f;
+
+    private Vector3 _mainCamPos;
+    private Vector3 destination;
+    
+    Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = GameObject.Find("Destination").transform.position;
+        dir = (destination - transform.position).normalized;
+        transform.position = Vector3.LerpUnclamped(
+            transform.position,
+            Camera.main.transform.position,
+            Random.Range(-.25f, .25f)
+        );
+        float offset = 2;
+        transform.Translate(
+            ((Random.value * 2) - 1) * offset,
+            ((Random.value * 2) - 1) * offset, 
+            0
+        );
+        speed = speed * Random.Range(.5f, 1f);
+        scaleIncrease *= (Random.value - .25f) * .01f;
+        transform.localScale = Vector3.one * Random.Range(.5f, 2f);
+       
+        _mainCamPos = Camera.main.transform.position;
+
+    }
+
+    // // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            transform.position += dir * speed  * Time.deltaTime;
+            transform.localScale += Vector3.one * scaleIncrease;
+            
+            if(wobble) {
+                float sin = (Mathf.Sin(Time.time) + 1) / 2;
+                float dist = .25f;
+                Camera.main.transform.position = _mainCamPos + new Vector3(
+                    Mathf.Lerp(-dist, dist, sin),0,0
+                );
+            }
+        }
+    }
+}
