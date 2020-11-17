86da9b004cdbdf49f499b8ce053008ad648ad505
rexchao27 <rexchao27@gmail.com>
2020-11-17 15:09:32 +0800

    shoot

---
+++ L1-2/Assets/ballgame/scripts/DeleteObjectOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObjectOnCollision : MonoBehaviour
+{
+    public string otherTag;
+    void OnTriggerEnter(Collider c){
+        if (c.gameObject.tag == otherTag) {     
+            GameObject.Destroy(c.gameObject);
+        }
+    }
+}
