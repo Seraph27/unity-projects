09608190b02e0157e42e701e88451e9d7c46f312
rexchao27 <rexchao27@gmail.com>
2020-11-27 17:25:22 +0800

    friday changed

--- L1-2/Assets/ballgame/scripts/BulletManager.cs
+++ L1-2/Assets/ballgame/scripts/BulletManager.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class BulletManager : MonoBehaviour
 {
-    public GameObject toSpawn;
+    public GameObject spawnBullet;
     public string key;
     public int bulletCount;
+    GameObject bulletAmountIcon;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletAmountIcon = GameObject.Find("bulletAmount");
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(key) && bulletCount > 0){
-            Instantiate(toSpawn, transform.position + new Vector3(1,0,0), Quaternion.identity);
+            Instantiate(spawnBullet, transform.position + new Vector3(1.5f,0,0), Quaternion.identity);
+            bulletAmountIcon.transform.position += new Vector3(2, 0, 0);
             bulletCount--;
             print(bulletCount);
         }
@@ -29,6 +31,7 @@ public class BulletManager : MonoBehaviour
         if (c.gameObject.tag == "Gem") {
             if (bulletCount < 3){
                 bulletCount++;
+                bulletAmountIcon.transform.position -= new Vector3(2, 0, 0);
                 print(bulletCount);
             }
 
