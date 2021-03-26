d19b347ec37ac839e2cd01871dd7dd9303b540c2
rexchao27 <rexchao27@gmail.com>
2021-03-26 17:28:22 +0800

    enemy ideas and shifitng enemy

--- project1/Assets/Scripts/EnemyController.cs
+++ project1/Assets/Scripts/EnemyController.cs
@@ -28,7 +28,7 @@ abstract public class EnemyController : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D c){
+    void OnTriggerEnter2D(Collider2D c){  //player dealt dmg
         if (c.gameObject.tag == "PlayerProjectile") {
             GameObject player = GameObject.Find("Player(Clone)");
             if (player != null){
@@ -40,3 +40,60 @@ abstract public class EnemyController : MonoBehaviour
     }
 
 }
+
+
+
+
+
+/*
+
+ideas:
+just one scene and teleport pads transports player to another area? where they don't see cause everytime i make something on scene 1 i need to copy to scene 2
+
+
+
+enemy ideas:
+
+shifing enemy:  (DONE)
+have a certain chance to shift itself in its radius if the enemy detects a projectile is going to hit it   
+
+Meteor enemy:
+
+drops a meteor and it shows a red circle of where its gonna land, so player needs to leave the area or they will take dmg
+
+Trail enemy:
+
+leaves a trail of poison anywhere it goes and does dmg when player steps on it
+
+dashing enemy:
+
+follows the player and dashes through the player when its close enough doing damage
+
+earthquake enemy:
+
+hits the ground and send spikes of rocks out of all directions dealing damage
+
+sHIELD enemy:
+
+generates a shield around it that takes 5 hits to destroy and reflects the bullet back at the player
+
+shockwave enemy:
+
+sends out a shockwave that pushes the player away
+
+gravity enemy:
+
+sucks the player in and explodes on it
+
+
+
+Boss:
+
+larger hp bar (or a health bar that shows up at the top of the screen with exact health point and stuff)
+single room 
+use multiple skills from previous enemy
+
+
+
+
+*/
---
+++ project1/Assets/Scripts/ShiftingEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftingEnemy : EnemyController
+{
+
+
+    override public int GetBaseHp(){
+        return 200;
+    }
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D c){ 
+
+        if (c.gameObject.tag == "PlayerProjectile") {
+            float triggerChance = Random.Range(0, 100);
+            print(triggerChance);
+            if(triggerChance > 50){
+                Vector3 randomVec3 = new Vector3(Random.Range(-4f, 4f), Random.Range(-4f, 4f), 0);
+                transform.position += randomVec3;
+            }
+            else{
+                GameObject player = GameObject.Find("Player(Clone)");
+                if (player != null){
+                    PlayerController playerController = player.GetComponent<PlayerController>();
+                    hpBarScript.ApplyDamage(25 * playerController.damageMultiplier);
+                } 
+                Destroy(c.gameObject);
+            }
+        }
+    }
+}
