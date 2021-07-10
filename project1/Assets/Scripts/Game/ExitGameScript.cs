b4cbff9ca1c9eaf00aabf8f2cef3958a00d6374e
rexchao27 <rexchao27@gmail.com>
2021-07-10 23:12:52 +0800

    new custom menu icons

---
+++ project1/Assets/Scripts/Game/ExitGameScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown() {
+        doExitGame();
+    }
+
+    void doExitGame() {
+        Debug.Log("EXITING");
+        Application.Quit();
+    }
+}
