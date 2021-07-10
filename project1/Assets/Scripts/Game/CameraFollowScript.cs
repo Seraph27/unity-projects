39721e8f1ff7b57fa864ea2731ac146157f45510
rexchao27 <rexchao27@gmail.com>
2021-07-11 00:40:15 +0800

    exit message and image xd

--- project1/Assets/Scripts/Game/CameraFollowScript.cs
+++ project1/Assets/Scripts/Game/CameraFollowScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollowScript : MonoBehaviour
 {
     GameObject player;
-    public int depth;
+    public int depth = -10;
     // Start is called before the first frame update
     void Start()
     {
